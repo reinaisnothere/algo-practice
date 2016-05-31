390c0659d2b0a53be91a5f8ab0192f43155355c9
Reina Mei <reinameic@gmail.com>
2016-05-31 13:13:31 -0700

    Add helper function to convert indices

--- search2DMatrix.py
+++ search2DMatrix.py
@@ -1,10 +1,19 @@
+def convertFlatIndexToPositionInMatrix(flatIndex, numColumns):
+  # flatIndex starts at 1
+  if flatIndex % numColumns == 0:
+    return (flatIndex / numColumns - 1, numColumns - 1)
+  else:
+    return (flatIndex // numColumns, flatIndex % numColumns -1)
+
 class Solution(object):
   def searchMatrix(self, matrix, target):
+    numColumns = len(matrix[0])
     flatLength = len(matrix) * len(matrix[0])
-    highFlatIndex = flatLength - 1
-    lowFlatIndex = 0
+    highFlatIndex = flatLength
+    lowFlatIndex = 1
     midFlatIndex = (highFlatIndex + lowFlatIndex) // 2
-    
+
         
 
 
+print convertFlatIndexToPositionInMatrix(5, 5)
