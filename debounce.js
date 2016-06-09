bb0c3c2c30db566872e35ceb112aac3a433c5d04
Reina Mei <reinameic@gmail.com>
2016-06-08 23:58:38 -0700

    Improve debounce

--- debounce.js
+++ debounce.js
@@ -1,36 +1,27 @@
 var debounce = function(func, wait, immediate) {
   var timeout;
   var result;
-  var lastCalled;
   var context;
   var args;
-  var firstInvocation = true;
 
   function reset() {
-    result = func.apply(context, args);
+    if (!immediate) {
+      result = func.apply(context, args);
+    }
     timeout = null;
   }
 
   return function() {
     args = Array.prototype.slice.apply(arguments);
     context = this;
-    if (firstInvocation) {
-      if (immediate) {
-        result = func.apply(context, args);
-      } else {
-        timeout = setTimeout(reset, wait);
-      }
-      firstInvocation = false;
-    } else {
-      if (!timeout) {
-        if (Date.now - lastCalled > wait) {
-          result = func.apply(context, args);
-        } else {
-          timeout = setTimeout(reset, wait - (Date.now - lastCalled));
-        }
-      }
+    var callNow = immediate && (!timeout);
+    if (timeout) {
+      clearTimeout(timeout);
     }
-    lastCalled = Date.now;
+    if (callNow) {
+      result = func.apply(context, args);
+    }
+    timeout = setTimeout(reset, wait);
     return result;
   }
 };
