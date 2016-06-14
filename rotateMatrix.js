d36e59bd02436a944215fad9fb7c5519756057e6
Reina Mei <reinameic@gmail.com>
2016-06-14 13:04:27 -0700

    Implement rotateMatrix

--- rotateMatrix.js
+++ rotateMatrix.js
@@ -1,22 +1,26 @@
 var assert = require('assert');
 
-function rotateMatrix(matrix) {
+function rotateMatrix(matrix) { // rotate clockwise
   var output = [];
   for (var i = 0; i < matrix.length; i++) { // i is row index
     for (var j = 0; j < matrix[0].length; j++) { // j is column index
-
+      if (!output[j]) {
+        output[j] = [];
+      }
+      output[j][matrix.length - 1 - i] = matrix[i][j];
     }
   }
+  return output;
 }
 
 function testRotateMatrix() {
   var matrix0 = [];
   assert.deepEqual(rotateMatrix(matrix0), []);
-  var matrix1 = [
+  var matrix1 = [ //(0, 1)
     [1, 2, 3], 
     [4, 5, 6]
   ];
-  assert.deepEqual(rotateMatrix(matrix1), [[4, 1], [5, 2], [6, 3]]);
+  assert.deepEqual(rotateMatrix(matrix1), [[4, 1], [5, 2], [6, 3]]); //(1, 1)
 }
 
 testRotateMatrix();
